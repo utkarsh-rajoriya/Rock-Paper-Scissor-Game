5d9a341bca86b7ed17a135771d69345b2b235f48
Utkarsh Rajoriya <utkarshrajoriya00@gmail.com>
2025-04-22 11:12:04 +0530

    ui updated

--- src/App.js
+++ src/App.js
@@ -1,105 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import rockImg from './Components/rock.png';
+import paperImg from './Components/paper.png';
+import scissorImg from './Components/scissors.png';
+
 export default function App() {
+  const [userScore, setUserScore] = useState(0);
+  const [compScore, setCompScore] = useState(0);
+  const [message, setMessage] = useState("Play your move");
 
-  let getCompchoice = () =>{
-    let arr = ["scissor", "paper" , "rock"]
-    let rndind = Math.floor(Math.random() * 3);
-   return arr[rndind];
-  }
+  const getCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    return options[Math.floor(Math.random() * 3)];
+  };
 
-  let compScore = 0;
-  let userScore = 0;
+  const playRound = (userChoice) => {
+    const compChoice = getCompChoice();
 
-  let RockClick = () =>{
-    // user = rock
-   let compChoice =  getCompchoice();
-   console.log("user: rock")
-   console.log("comp: "+ compChoice)
+    if (
+      (userChoice === "rock" && compChoice === "scissor") ||
+      (userChoice === "paper" && compChoice === "rock") ||
+      (userChoice === "scissor" && compChoice === "paper")
+    ) {
+      setUserScore(userScore + 1);
+      setMessage(`You Win! ${userChoice} beats ${compChoice}`);
+    } else if (userChoice === compChoice) {
+      setMessage("It's a Draw!");
+    } else {
+      setCompScore(compScore + 1);
+      setMessage(`You Lose! ${compChoice} beats ${userChoice}`);
+    }
+  };
 
-   if(compChoice === "paper" ){
-    document.body.querySelector('.msg').innerText ="You Lose";
-    compScore = compScore + 1 ;
-    document.body.querySelector('.comp-score').innerText =`${compScore}`
-   }
-   else if(compChoice ==='scissor'){
-    document.body.querySelector('.msg').innerText ="You Win";
-    userScore = userScore + 1 ;
-    document.body.querySelector('.user-score').innerText =`${userScore}`
-   }
-   else{
-    document.body.querySelector('.msg').innerText ="Draw";
-   }
-
-  }
-
-  let PaperClick = () =>{
-    // user = paper
-   let compChoice =  getCompchoice();
-   console.log("user: paper")
-   console.log("comp: "+ compChoice)
-
-   if(compChoice === "scissor" ){
-    document.body.querySelector('.msg').innerText ="You Lose";
-    compScore = compScore + 1 ;
-    document.body.querySelector('.comp-score').innerText =`${compScore}`
-   }
-   else if(compChoice ==='rock'){
-    document.body.querySelector('.msg').innerText ="You Win";
-    userScore = userScore + 1 ;
-    document.body.querySelector('.user-score').innerText =`${userScore}`
-   }
-   else{
-    document.body.querySelector('.msg').innerText ="Draw";
-   }
-
-  }
-
-  let ScissorClick = () =>{
-    // user = scissor
-   let compChoice =  getCompchoice();
-   console.log("user: scissor")
-   console.log("comp: "+ compChoice)
-
-   if(compChoice === "rock" ){
-    document.body.querySelector('.msg').innerText ="You Lose";
-    compScore = compScore + 1 ;
-    document.body.querySelector('.comp-score').innerText =`${compScore}`
-   }
-   else if(compChoice ==='paper'){
-    document.body.querySelector('.msg').innerText ="You Win";
-    userScore = userScore + 1 ;
-    document.body.querySelector('.user-score').innerText =`${userScore}`
-   }
-   else{
-    document.body.querySelector('.msg').innerText ="Draw";
-   }
-
-  }
+  const resetGame = () => {
+    setUserScore(0);
+    setCompScore(0);
+    setMessage("Play your move");
+  };
 
   return (
-    <div>
-      <h1 className="rps-heading">Rock Paper Scissor</h1>
-
-      <div className="choices">
-        <div className="choice" id="rock" onClick={RockClick}></div>
-        <div className="choice" id="paper" onClick={PaperClick}></div>
-        <div className="choice" id="scissor" onClick={ScissorClick}></div>
-      </div>
+    <div className="container">
+      <h1 className="rps-heading">Rock Paper Scissors</h1>
 
       <div className="score-board">
         <div className="score">
-          <p className="user-score">0</p>
+          <p className="user-score">{userScore}</p>
           <p>You</p>
         </div>
         <div className="score">
-          <p className="comp-score">0</p>
-          <p>Comp</p>
+          <p className="comp-score">{compScore}</p>
+          <p>Computer</p>
         </div>
       </div>
 
+      <div className="choices">
+      <img src={rockImg} alt="rock" className="choice-img" onClick={() => playRound("rock")} />
+      <img src={paperImg} alt="paper" className="choice-img" onClick={() => playRound("paper")} />
+      <img src={scissorImg} alt="scissor" className="choice-img" onClick={() => playRound("scissor")} />
+      </div>
+
       <div className="msg-container">
-        <p className="msg">Play your move</p>
+        <p className="msg">{message}</p>
+      </div>
+
+      <div className="reset-container">
+        <button className="reset-btn" onClick={resetGame}>Reset</button>
       </div>
     </div>
   );
